27a44778dfe41b1c313bb032ceac181165155f07
Fernando Cejas <fcejas@gmail.com>
2017-01-12 12:36:26 +0100

    Add tests for Serializer.

--- app/src/test/kotlin/com/fernandocejas/android/sample/SerializerTest.kt
+++ app/src/test/kotlin/com/fernandocejas/android/sample/SerializerTest.kt
@@ -16,13 +16,48 @@
 package com.fernandocejas.android.sample
 
 import org.amshove.kluent.shouldEqual
+import org.junit.Before
 import org.junit.Test
 
 class SerializerTest {
 
+    private val JSON_RESPONSE = "{\n \"id\": 1,\n " +
+                                "\"cover_url\": \"http://www.android10.org/myapi/cover_1.jpg\",\n " +
+                                "\"full_name\": \"Simon Hill\",\n " +
+                                "\"description\": \"Curabitur gravida nisi at nibh. In hac habitasse " +
+                                "platea dictumst. Aliquam augue quam, sollicitudin vitae, consectetuer " +
+                                "eget, rutrum at, lorem.\\n\\nInteger tincidunt ante vel ipsum. " +
+                                "Praesent blandit lacinia erat. Vestibulum sed magna at nunc commodo " +
+                                "placerat.\\n\\nPraesent blandit. Nam nulla. Integer pede justo, " +
+                                "lacinia eget, tincidunt eget, tempus vel, pede.\",\n " +
+                                "\"followers\": 7484,\n " +
+                                "\"email\": \"jcooper@babbleset.edu\"\n}"
+
+    private lateinit var serializer: Serializer
+
+    @Before
+    fun setUp() {
+        serializer = Serializer()
+    }
+
     @Test
-    fun testSomething() {
-        true shouldEqual true
+    fun shouldSerialize() {
+        val userEntityOne = serializer.deserialize(JSON_RESPONSE, UserEntity::class.java)
+        val jsonString = serializer.serialize(userEntityOne, UserEntity::class.java)
+        val userEntityTwo = serializer.deserialize(jsonString, UserEntity::class.java)
+
+        userEntityOne.userId shouldEqual userEntityTwo.userId
+        userEntityOne.fullname shouldEqual userEntityTwo.fullname
+        userEntityOne.followers shouldEqual userEntityTwo.followers
+    }
+
+    @Test
+    fun shouldDesearialize() {
+        val userEntity = serializer.deserialize(JSON_RESPONSE, UserEntity::class.java)
+
+        userEntity.userId shouldEqual 1
+        userEntity.fullname shouldEqual "Simon Hill"
+        userEntity.followers shouldEqual 7484
     }
 }
 
